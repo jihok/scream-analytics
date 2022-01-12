2d1af3836b5a5e222f2671f04db51ce8d41ea39c
jihok <1700785+jihok@users.noreply.github.com>
2022-01-11 21:20:42 -0500

    add mobile pagination to table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,7 +2,14 @@ import React, { useMemo } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { formatAbbrUSD } from '../utils/Market';
-import { useTable, Column, useSortBy, Row } from 'react-table';
+import {
+  useTable,
+  Column,
+  useSortBy,
+  Row,
+  usePagination,
+  UsePaginationInstanceProps,
+} from 'react-table';
 import { useMarketContext } from '../contexts/MarketContext';
 import PercentChange, { MutableData } from './PercentChange';
 
@@ -144,107 +151,154 @@ export default function Table() {
     [todayMarkets, yesterdayMarkets]
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable<TableData>(
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    // @ts-ignore
+    page,
+    prepareRow,
+    // @ts-ignore
+    canPreviousPage,
+    // @ts-ignore
+    canNextPage,
+    // @ts-ignore
+    pageOptions,
+    // @ts-ignore
+    pageCount,
+    // @ts-ignore
+    gotoPage,
+    // @ts-ignore
+    nextPage,
+    // @ts-ignore
+    previousPage,
+    // @ts-ignore
+    state: { pageIndex, pageSize },
+  } = useTable<TableData>(
     {
       columns,
       data: tableData,
       initialState: {
         // @ts-ignore
-        sortBy: [{ id: 'liquidity' }],
+        sortBy: [{ id: 'asset' }],
+        pageSize: 7,
       },
       getRowId: (row) => row.id,
     },
-    useSortBy
+    useSortBy,
+    usePagination
   );
-
+  console.log(canPreviousPage);
   return (
-    <table {...getTableProps()} className="mt-7 bg-darkGray rounded-md shadow-3xl">
-      <thead className="caption-label">
-        {headerGroups.map((headerGroup, i) => (
-          <tr
-            {...headerGroup.getHeaderGroupProps()}
-            key={headerGroup.headers[i].id}
-            className="border-border-primary border-b text-left"
-          >
-            {headerGroup.headers.map((column) => {
-              // @ts-ignore - UseSortByColumnOptions
-              column.sortType = (a: Row, b: Row, accessor: string) => {
-                if (accessor === 'asset') {
-                  return a.values[accessor].underlyingSymbol.toLowerCase() >
-                    b.values[accessor].underlyingSymbol.toLowerCase()
-                    ? 1
-                    : -1;
-                }
-                return a.values[accessor].todayVal > b.values[accessor].todayVal ? 1 : -1;
-              };
+    <>
+      <table {...getTableProps()} className="mt-7 bg-darkGray rounded-md shadow-3xl">
+        <thead className="caption-label">
+          {headerGroups.map((headerGroup, i) => (
+            <tr
+              {...headerGroup.getHeaderGroupProps()}
+              key={headerGroup.headers[i].id}
+              className="border-border-primary border-b text-left"
+            >
+              {headerGroup.headers.map((column) => {
+                // @ts-ignore - UseSortByColumnOptions
+                column.sortType = (a: Row, b: Row, accessor: string) => {
+                  if (accessor === 'asset') {
+                    return a.values[accessor].underlyingSymbol.toLowerCase() >
+                      b.values[accessor].underlyingSymbol.toLowerCase()
+                      ? 1
+                      : -1;
+                  }
+                  return a.values[accessor].todayVal > b.values[accessor].todayVal ? 1 : -1;
+                };
 
-              const getClassName = () => {
-                let res = 'w-fit whitespace-nowrap';
-                if (column.id === 'asset') {
-                  res += ' shadow-tableAsset';
-                }
-                if (column.id === 'liquidity') {
-                  res += ' hidden lg:table-cell';
-                }
-                return res;
-              };
-              return (
-                <th
-                  // @ts-ignore - UseSortByColumnProps
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  key={column.id}
-                  className={getClassName()}
-                >
-                  {column.render('Header')}
-                  <span
-                    style={{
-                      fontSize: 6,
-                      marginLeft: 3,
-                      width: 10,
-                      height: 10,
-                      display: 'inline-block',
-                    }}
+                const getClassName = () => {
+                  let res = 'w-fit whitespace-nowrap';
+                  if (column.id === 'asset') {
+                    res += ' shadow-tableAsset';
+                  }
+                  if (column.id === 'liquidity') {
+                    res += ' hidden lg:table-cell';
+                  }
+                  return res;
+                };
+
+                return (
+                  <th
+                    // @ts-ignore - UseSortByColumnProps
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    key={column.id}
+                    className={getClassName()}
                   >
-                    {/* @ts-ignore - UseSortByColumnProps */}
-                    {column.isSorted ? (column.isSortedDesc ? ' ▼' : ' ▲') : ' '}
-                  </span>
-                </th>
-              );
-            })}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <Link href={`market/${row.id}`} key={row.id} passHref>
-              <tr {...row.getRowProps()} style={{ cursor: 'pointer' }}>
-                {row.cells.map((cell) => {
-                  const getClassName = () => {
-                    if (cell.column.id === 'asset') {
-                      return 'shadow-tableAsset';
-                    }
-                    return cell.column.id === 'liquidity' ? 'hidden lg:table-cell' : '';
-                  };
-                  return (
-                    <td
-                      {...cell.getCellProps()}
-                      className={getClassName()}
-                      key={`${cell.row.id}-${cell.column.id}`}
+                    {column.render('Header')}
+                    <span
+                      style={{
+                        fontSize: 6,
+                        marginLeft: 3,
+                        width: 10,
+                        height: 10,
+                        display: 'inline-block',
+                      }}
                     >
-                      {
-                        // @ts-ignore - type def should but doesn't include JSX.Element
-                        cell.render(<CustomCell colId={cell.column.id} val={cell.value} />)
+                      {/* @ts-ignore - UseSortByColumnProps */}
+                      {column.isSorted ? (column.isSortedDesc ? ' ▼' : ' ▲') : ' '}
+                    </span>
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row: any) => {
+            prepareRow(row);
+            return (
+              <Link href={`market/${row.id}`} key={row.id} passHref>
+                <tr {...row.getRowProps()} style={{ cursor: 'pointer' }}>
+                  {row.cells.map((cell: any) => {
+                    const getClassName = () => {
+                      if (cell.column.id === 'asset') {
+                        return 'shadow-tableAsset';
                       }
-                    </td>
-                  );
-                })}
-              </tr>
-            </Link>
-          );
-        })}
-      </tbody>
-    </table>
+                      return cell.column.id === 'liquidity' ? 'hidden lg:table-cell' : '';
+                    };
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+                        className={getClassName()}
+                        key={`${cell.row.id}-${cell.column.id}`}
+                      >
+                        {cell.render(<CustomCell colId={cell.column.id} val={cell.value} />)}
+                      </td>
+                    );
+                  })}
+                </tr>
+              </Link>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="flex justify-center mt-5">
+        <button
+          className="px-3 bg-border-primary rounded-l-full border border-white disabled:bg-border-secondary"
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          ◀
+        </button>
+        <span className="px-4 py-3 border-t border-b border-border-primary">
+          {pageIndex * pageSize + 1} - {Math.min(pageIndex * pageSize + 7, tableData.length)} of{' '}
+          {tableData.length}
+        </span>
+        <button
+          className="px-3 bg-bar-6 rounded-r-full border border-white"
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
+          ▶
+        </button>
+      </div>
+    </>
   );
 }
+
+const PageTurner = () => {};
