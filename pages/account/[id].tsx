ab4763e65141de1c9a71d498ed610c680f52af25
jihok <1700785+jihok@users.noreply.github.com>
2022-01-14 13:25:07 -0500

    account styling + simulator in header

--- pages/account/[id].tsx
+++ pages/account/[id].tsx
@@ -8,7 +8,6 @@ import { RawAccount, transformAccountData } from '../../src/utils/Account';
 import AccountHeader from '../../src/components/Account/Header';
 import { BORROW_COLOR, SUPPLY_COLOR } from '../../src/components/Market/UtilizationChart';
 import { usdFormatter } from '../../src/utils/Market';
-import HealthSimulator from '../../src/components/Account/HealthSimulator';
 
 const REPAID_COLOR = '#89DFDB'; // also equal to bg-bar-0
 
@@ -20,7 +19,6 @@ export default function Account() {
     variables: { id },
   });
   const [overviewType, setOverviewType] = useState<'supplied' | 'borrowed'>('supplied');
-  const [showSimulator, setShowSimulator] = useState(false);
 
   if (loading || !data) return <p>Loading</p>;
   if (error) return <p>Error :(</p>;
@@ -42,148 +40,138 @@ export default function Account() {
   const account = transformAccountData(data.accounts[0]);
 
   return (
-    <>
-      <Layout className={`p-5 lg:px-80 ${showSimulator && 'fixed'}`}>
-        <AccountHeader account={account} />
+    <Layout className={`p-5 lg:px-80`}>
+      <AccountHeader account={account} />
 
-        <h3 className="pt-10 pb-4">Account overview</h3>
-        <button onClick={() => setShowSimulator(true)}>Simulator</button>
-        <div className="flex text-center mb-4 cursor-pointer">
-          <p
-            className={`${
-              overviewType === 'supplied'
-                ? 'border-b-2 border-border-active font-sans-semibold'
-                : 'border-b border-border-primary'
-            }  w-half pb-3`}
-            onClick={() => setOverviewType('supplied')}
-          >
-            Supply
-          </p>
-          <p
-            className={`${
-              overviewType === 'borrowed'
-                ? 'border-b-2 border-border-active font-sans-semibold'
-                : 'border-b border-border-primary'
-            }  w-half pb-3`}
-            onClick={() => setOverviewType('borrowed')}
-          >
-            Borrow
-          </p>
-        </div>
-        <p className="mb-5">Current state {overviewType}</p>
-        <div className="flex flex-col mb-8">
-          {account.tokens.map((token) => {
-            if (!token.cTokenBalance && overviewType === 'supplied') return;
-            if (overviewType === 'borrowed' && !token.storedBorrowBalance) return;
-
-            const valueUSD =
-              overviewType === 'supplied'
-                ? token.cTokenBalance * token.market.exchangeRate * token.market.underlyingPrice
-                : token.storedBorrowBalance * token.market.underlyingPrice;
-
-            const repaid =
-              ((token.totalUnderlyingRepaid * token.market.underlyingPrice) /
-                account.totalBorrowedUSD) *
-              100;
-            const barWidthPercent =
-              overviewType === 'supplied'
-                ? (valueUSD / account.totalSuppliedUSD) * 100
-                : ((token.totalUnderlyingBorrowed * token.market.underlyingPrice) /
-                    account.totalBorrowedUSD) *
-                    100 -
-                  repaid;
-
-            return (
-              <div className="flex flex-row items-center" key={token.symbol}>
-                <div
-                  className={`pr-6 ${overviewType === 'borrowed' && 'pb-8'}`}
-                  style={{ minWidth: 100, maxWidth: 100 }}
-                >
-                  <p className="text-label lg:text-body">{token.market.underlyingSymbol}</p>
-                  <p className="font-sans-semibold">{token.market.underlyingName}</p>
-                </div>
-                <div className="border-border-primary border-l py-4 w-full">
-                  <div className="flex">
-                    {overviewType === 'borrowed' && (
-                      <div
-                        style={{
-                          width: `${repaid}%`,
-                          backgroundColor: REPAID_COLOR,
-                          height: 24,
-                        }}
-                      />
-                    )}
-                    <div style={{ width: `${barWidthPercent}%` }}>
-                      <div
-                        style={{
-                          backgroundColor:
-                            overviewType === 'supplied' ? SUPPLY_COLOR : BORROW_COLOR,
-                          borderRadius: '0px 3px 3px 0px',
-                          height: 24,
-                        }}
-                      />
-                      {overviewType === 'borrowed' && (
-                        <>
-                          <p className="text-caption font-sans-semibold pt-2">Repaid</p>
-                          <p className="text-label">
-                            {usdFormatter.format(
-                              token.totalUnderlyingRepaid * token.market.underlyingPrice
-                            )}
-                          </p>
-                        </>
-                      )}
-                    </div>
-                    <div
-                      style={{
-                        backgroundColor: '#31333799',
-                        borderRadius: '0px 3px 3px 0px',
-                        height: 24,
-                        width: `${100 - barWidthPercent - repaid}%`,
-                      }}
-                    />
-                  </div>
-                </div>
-                <div
-                  className={`flex flex-col items-end pl-3 ${
-                    overviewType === 'borrowed' && 'pb-11'
-                  }`}
-                  style={{ minWidth: 80, width: 'fit-content' }}
-                >
-                  {overviewType === 'borrowed' && (
-                    <p className="text-caption font-sans-semibold pt-2 text-right">Remaining</p>
-                  )}
-                  <p className="text-label">${valueUSD.toFixed(2)}</p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-
-        <p className="mb-4">Lifetime {overviewType} interest accrued</p>
-        {account.tokens.map((token) => {
-          const interest =
+      <h3 className="pt-10 pb-4">Account overview</h3>
+      <div className="flex text-center mb-4 cursor-pointer">
+        <p
+          className={`${
             overviewType === 'supplied'
-              ? (token.cTokenBalance * token.market.exchangeRate -
-                  token.totalUnderlyingSupplied +
-                  token.totalUnderlyingRedeemed) *
-                token.market.underlyingPrice
-              : (token.storedBorrowBalance * token.market.borrowIndex) / token.accountBorrowIndex -
-                token.totalUnderlyingBorrowed +
-                token.totalUnderlyingRepaid;
-          if (!interest) return;
+              ? 'border-b-2 border-border-active font-sans-semibold'
+              : 'border-b border-border-primary'
+          }  w-half pb-3`}
+          onClick={() => setOverviewType('supplied')}
+        >
+          Supply
+        </p>
+        <p
+          className={`${
+            overviewType === 'borrowed'
+              ? 'border-b-2 border-border-active font-sans-semibold'
+              : 'border-b border-border-primary'
+          }  w-half pb-3`}
+          onClick={() => setOverviewType('borrowed')}
+        >
+          Borrow
+        </p>
+      </div>
+      <div className="flex flex-col mb-8">
+        {account.tokens.map((token) => {
+          if (!token.cTokenBalance && overviewType === 'supplied') return;
+          if (overviewType === 'borrowed' && !token.storedBorrowBalance) return;
+
+          const valueUSD =
+            overviewType === 'supplied'
+              ? token.cTokenBalance * token.market.exchangeRate * token.market.underlyingPrice
+              : token.storedBorrowBalance * token.market.underlyingPrice;
+
+          const repaid =
+            ((token.totalUnderlyingRepaid * token.market.underlyingPrice) /
+              account.totalBorrowedUSD) *
+            100;
+          const barWidthPercent =
+            overviewType === 'supplied'
+              ? (valueUSD / account.totalSuppliedUSD) * 100
+              : ((token.totalUnderlyingBorrowed * token.market.underlyingPrice) /
+                  account.totalBorrowedUSD) *
+                  100 -
+                repaid;
 
           return (
-            <div className="flex whitespace-nowrap mb-3" key={token.symbol}>
-              <p>{token.market.underlyingSymbol}</p>
-              <div className="border-b border-border-secondary w-full self-center mx-2" />
-              <p className="font-sans-semibold">${interest.toFixed(2)}</p>
+            <div className="flex flex-row items-center" key={token.symbol}>
+              <div
+                className={`pr-6 ${overviewType === 'borrowed' && 'pb-8'}`}
+                style={{ minWidth: 100, maxWidth: 100 }}
+              >
+                <p className="text-label lg:text-body">{token.market.underlyingSymbol}</p>
+                <p className="font-sans-semibold">{token.market.underlyingName}</p>
+              </div>
+              <div className="border-border-primary border-l py-4 w-full">
+                <div className="flex">
+                  {overviewType === 'borrowed' && (
+                    <div
+                      style={{
+                        width: `${repaid}%`,
+                        backgroundColor: REPAID_COLOR,
+                        height: 24,
+                      }}
+                    />
+                  )}
+                  <div style={{ width: `${barWidthPercent}%` }}>
+                    <div
+                      style={{
+                        backgroundColor: overviewType === 'supplied' ? SUPPLY_COLOR : BORROW_COLOR,
+                        borderRadius: '0px 3px 3px 0px',
+                        height: 24,
+                      }}
+                    />
+                    {overviewType === 'borrowed' && (
+                      <>
+                        <p className="text-caption font-sans-semibold pt-2">Repaid</p>
+                        <p className="text-label">
+                          {usdFormatter.format(
+                            token.totalUnderlyingRepaid * token.market.underlyingPrice
+                          )}
+                        </p>
+                      </>
+                    )}
+                  </div>
+                  <div
+                    style={{
+                      backgroundColor: '#31333799',
+                      borderRadius: '0px 3px 3px 0px',
+                      height: 24,
+                      width: `${100 - barWidthPercent - repaid}%`,
+                    }}
+                  />
+                </div>
+              </div>
+              <div
+                className={`flex flex-col items-end pl-3 ${overviewType === 'borrowed' && 'pb-11'}`}
+                style={{ minWidth: 80, width: 'fit-content' }}
+              >
+                {overviewType === 'borrowed' && (
+                  <p className="text-caption font-sans-semibold pt-2 text-right">Remaining</p>
+                )}
+                <p className="text-label">${valueUSD.toFixed(2)}</p>
+              </div>
             </div>
           );
         })}
-      </Layout>
-      {showSimulator && (
-        <HealthSimulator account={account} onClose={() => setShowSimulator(false)} />
-      )}
-    </>
+      </div>
+
+      <p className="mb-4">Lifetime {overviewType} interest accrued</p>
+      {account.tokens.map((token) => {
+        const interest =
+          overviewType === 'supplied'
+            ? (token.cTokenBalance * token.market.exchangeRate -
+                token.totalUnderlyingSupplied +
+                token.totalUnderlyingRedeemed) *
+              token.market.underlyingPrice
+            : (token.storedBorrowBalance * token.market.borrowIndex) / token.accountBorrowIndex -
+              token.totalUnderlyingBorrowed +
+              token.totalUnderlyingRepaid;
+        if (!interest) return;
+
+        return (
+          <div className="flex whitespace-nowrap mb-3" key={token.symbol}>
+            <p>{token.market.underlyingSymbol}</p>
+            <div className="border-b border-border-secondary w-full self-center mx-2" />
+            <p className="font-sans-semibold">${interest.toFixed(2)}</p>
+          </div>
+        );
+      })}
+    </Layout>
   );
 }
--- src/components/Account/Header.tsx
+++ src/components/Account/Header.tsx
@@ -1,41 +1,56 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import { Account, getHealth } from '../../utils/Account';
+import HealthSimulator from './HealthSimulator';
 
 export default function AccountHeader({ account }: { account: Account }) {
+  const [showSimulator, setShowSimulator] = useState(false);
+
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-between">
-      <div className="flex pb-7">
-        <div style={{ width: 33, height: 33, position: 'relative' }}>
-          <Image src="/images/User.png" layout="fill" objectFit="contain" alt="account" />
-        </div>
-        <div className="pl-5">
-          <h1>
-            {`${account.id.slice(0, 6)}...${account.id.slice(-4)}`}
-            <button className="pl-5" onClick={() => navigator.clipboard.writeText(account.id)}>
-              <Image src="/images/Copy.png" width={13} height={15} alt="copy" />
+    <>
+      <div className="flex flex-col lg:flex-row lg:justify-between">
+        <div className="flex mb-4 pb-6 border-b border-border-primary lg:border-0">
+          <div style={{ width: 33, height: 33, position: 'relative' }}>
+            <Image src="/images/User.png" layout="fill" objectFit="contain" alt="account" />
+          </div>
+          <div className="pl-5">
+            <h1>
+              {`${account.id.slice(0, 6)}...${account.id.slice(-4)}`}
+              <button className="pl-5" onClick={() => navigator.clipboard.writeText(account.id)}>
+                <Image src="/images/Copy.png" width={13} height={15} alt="copy" />
+              </button>
+            </h1>
+            <p className="pt-3">
+              Health score{' '}
+              <span className="font-sans-semibold">
+                {getHealth(account.borrowLimitUSD, account.borrowBalanceUSD).toFixed(2)}
+              </span>
+            </p>
+
+            <button
+              className="border border-border-primary rounded-l-full rounded-r-full py-2 px-3 mt-3 text-body"
+              onClick={() => setShowSimulator(true)}
+            >
+              Try Simulator →
             </button>
-          </h1>
-          <p className="pt-3">
-            Health score{' '}
-            <span className="font-sans-semibold">
-              {getHealth(account.borrowLimitUSD, account.borrowBalanceUSD).toFixed(2)}
-            </span>
-          </p>
-        </div>
-      </div>
-      <div>
-        <h3 className="pb-3 border-b border-border-primary lg:hidden">Account stats</h3>
-        <div className="flex pt-4">
-          <div className="pr-6" style={{ maxWidth: 100 }}>
-            <div className="caption-label">Times liquidated</div>
-            <h2 className="py-1">{account.countLiquidated}</h2>
           </div>
-          <div className="pr-6" style={{ maxWidth: 100 }}>
-            <div className="caption-label">Liquidation count (others)</div>
-            <h2 className="py-1">{account.countLiquidator}</h2>
+        </div>
+        <div>
+          <div className="flex pt-4">
+            <div className="pr-6" style={{ maxWidth: 150 }}>
+              <div className="caption-body">Times liquidated</div>
+              <h2 className="py-1">{account.countLiquidated}</h2>
+            </div>
+            <div className="pr-6" style={{ maxWidth: 150 }}>
+              <div className="caption-body">Liquidation count</div>
+              <h2 className="py-1">{account.countLiquidator}</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {showSimulator && (
+        <HealthSimulator account={account} onClose={() => setShowSimulator(false)} />
+      )}
+    </>
   );
 }
