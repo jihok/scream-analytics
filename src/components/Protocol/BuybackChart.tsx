71e6813ab135537beda4e5dd4f7f1640312d9a46
jihok <1700785+jihok@users.noreply.github.com>
2022-01-24 15:35:34 -0500

    buyback in usd using current price

--- src/components/Protocol/BuybackChart.tsx
+++ src/components/Protocol/BuybackChart.tsx
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import { useState } from 'react';
-import { ResponsiveContainer, BarChart, YAxis, Tooltip, Bar } from 'recharts';
+import { useGlobalContext } from '../../contexts/GlobalContext';
+import { ResponsiveContainer, BarChart, YAxis, Bar } from 'recharts';
 import { CategoricalChartState } from 'recharts/types/chart/generateCategoricalChart';
 import { Buyback } from '../../utils';
 import { SUPPLY_COLOR } from '../Market/UtilizationChart';
+import { usdFormatter } from '../../utils/Market';
 
 interface Props {
   data: Buyback[];
@@ -11,8 +13,8 @@ interface Props {
 
 export default function BuybackChart({ data }: Props) {
   const [focusedBar, setFocusedBar] = useState<number | undefined>(data.length - 1);
-  console.log(data);
-  console.log(focusedBar);
+  const { screamPrice } = useGlobalContext();
+
   return (
     <>
       <ResponsiveContainer width="100%" height={200}>
@@ -33,34 +35,17 @@ export default function BuybackChart({ data }: Props) {
             : '--'}
         </div>
         <div className="px-5 py-3 border-l border-border-secondary">
-          <div className="pb-1" style={{ color: SUPPLY_COLOR }}>
-            SCREAM
-          </div>
+          <div className="pb-1">SCREAM</div>
           <p>{focusedBar !== undefined ? data[focusedBar].amount.toFixed(2) : '--'}</p>
         </div>
-        {/*
         <div className="px-5 py-3 border-l border-border-secondary">
-          <div className="pb-1" style={{ color: BORROW_COLOR }}>
-            Total borrowed
-          </div>
-          <p>
-            {focusedBar !== undefined ? formatAbbrUSD(data[focusedBar]?.totalBorrowsUSD) : '--'}
-          </p>
-        </div>
-        <div className="px-5 py-3 border-l border-border-secondary">
-          <div className="pb-1" style={{ color: RESERVE_COLOR }}>
-            Reserves
-          </div>
-          <p>{focusedBar !== undefined ? formatAbbrUSD(data[focusedBar]?.reserves) : '--'}</p>
-        </div>
-        <div className="px-5 py-3 border-l border-border-secondary">
-          <div className="pb-1 text-secondary">Liquidity</div>
+          <div className="pb-1">USD</div>
           <p>
             {focusedBar !== undefined
-              ? formatAbbrUSD(+data[focusedBar]?.cash * data[focusedBar]?.underlyingPrice)
+              ? usdFormatter.format(data[focusedBar].amount * screamPrice)
               : '--'}
           </p>
-        </div> */}
+        </div>
       </div>
     </>
   );
