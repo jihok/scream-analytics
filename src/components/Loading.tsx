2d4741af9979cfc4888e2262079bc13510e2e9b5
jihok <1700785+jihok@users.noreply.github.com>
2022-01-25 13:42:18 -0500

    fix Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ import Layout from './Layout';
 export default function Loading() {
   return (
     <Layout className="flex h-screen justify-center">
-      <div className="absolute self-center loading top-1/3">
+      <div className="fixed self-center loading top-1/3">
         <Image src="/scream-blue-pink.png" width={200} height={200} alt="loading" priority />
       </div>
     </Layout>
